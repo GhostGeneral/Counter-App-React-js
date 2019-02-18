0b89f94c5497a6cc44dc9c22d197d02325acd6cf
Afeez Awotundun <afeezawotundun@Mrs-MacBook-Pro.local>
2019-02-18 14:14:25 +0100

    my counter app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={logo} className="App-logo" alt="logo" />  
+        <h2>My First React App - Counter</h2>
         </header>
+         <Counter initialNumber={0}/>
+         <Counter initialNumber={-10}/>
+         <Counter initialNumber={5}/>
+         <Counter initialNumber={30}/>
+       
       </div>
     );
   }
